2fc97aecc480ece0238731d052f866931bd23f6d
Seulji-jo <joseuling@gmail.com>
2024-02-04 00:53:30 +0900

    chore: 추가적인 설정 및 주석 설명 추가

--- zoom/src/server.js
+++ zoom/src/server.js
@@ -6,6 +6,8 @@ app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (req, res) => res.render("home"));
+// catchall url: 유저가 어떤 url로 이동하던지 홈으로 리다이렉트(해당 url만 사용)
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 app.listen(3000, handleListen);
