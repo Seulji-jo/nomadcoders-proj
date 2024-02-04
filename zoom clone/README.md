83c8990fb5e22513746f72a92aa0b7ad0dcb2d21
Seulji-jo <joseuling@gmail.com>
2024-02-04 23:51:42 +0900

    set: websocket 셋업 완

--- zoom clone/README.md
+++ zoom clone/README.md
@@ -13,4 +13,3 @@ Zoom Clone using Node.js, WebRTC and Websockets.
 3. 채팅방 컨셉 정하기
 4. 입장 & 퇴장 이벤트 추가 및 방 참여 인원수 확인
 5. 서버에 방이 실시간으로 몇 개 존재하는지 체크
-   ``
--- zoom clone/src/server.js
+++ zoom clone/src/server.js
@@ -1,3 +1,5 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 
 const app = express();
@@ -10,4 +12,19 @@ app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+// app.listen(3000, handleListen);
+
+// it's not require!!
+// 현 프로젝트의 경우에는 http & ws 같은 port에 두기 위한 방식
+// express로 같은 서버에 http 위에 ws 서버 생성 -> 동일한 포트에서 두가지 프로토콜 처리 가능
+// 요점은 내 http 서버에 access하려는 것
+// express는 ws를 지원 안하기 때문에 하기와 같이 function을 생성해 설정해아한다.
+
+// http 서버 생성
+const server = http.createServer(app);
+
+// http서버도 같이 돌려야할 경우 위에서 생성한 http서버 전달
+// http 서버 위에 webSocket 서버 생성
+const wss = new WebSocket.Server({ server });
+
+server.listen(3000, handleListen);
