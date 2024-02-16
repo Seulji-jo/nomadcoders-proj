157578cbebf281b502cf92ea74004ac6dc269474
Seulji-jo <joseuling@gmail.com>
2024-02-17 03:19:41 +0900

    3-1.UserVideo: 웹캠 화면에 그리기

--- zoom clone/src/public/js/app.js
+++ zoom clone/src/public/js/app.js
@@ -1,2 +1,47 @@
 // io는 자동적으로 back-end socket.io와 연결해주는 fn이다.
 const socket = io();
+
+const $myFace = document.getElementById("myFace");
+const $muteBtn = document.getElementById("mute");
+const $cameraBtn = document.getElementById("camera");
+
+// stream = video + audio
+let myStream;
+let isMuted = false;
+let isCameraOn = false;
+async function getMedia() {
+  // getUserMedia는 유저의 유저미디어 string을 준다
+  try {
+    myStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    $myFace.srcObject = myStream;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getMedia();
+
+function handleMuteClick() {
+  if (!isMuted) {
+    $muteBtn.innerText = "Unmute";
+    isMuted = true;
+  } else {
+    $muteBtn.innerText = "Mute";
+    isMuted = false;
+  }
+}
+function handleCameraClick() {
+  if (isCameraOn) {
+    $cameraBtn.innerText = "Camera Off";
+    isCameraOn = false;
+  } else {
+    $cameraBtn.innerText = "Camera On";
+    isCameraOn = true;
+  }
+}
+
+$muteBtn.addEventListener("click", handleMuteClick);
+$cameraBtn.addEventListener("click", handleCameraClick);
