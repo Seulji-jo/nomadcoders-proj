6ff124040b9c4ddc3fa2815996c672f04366a9f0
Seulji-jo <joseuling@gmail.com>
2024-02-17 03:39:42 +0900

    3-2 Call Controls: 카메라 선택 박스 생성

--- zoom clone/src/public/js/app.js
+++ zoom clone/src/public/js/app.js
@@ -4,11 +4,30 @@ const socket = io();
 const $myFace = document.getElementById("myFace");
 const $muteBtn = document.getElementById("mute");
 const $cameraBtn = document.getElementById("camera");
+const $cameraSelect = document.getElementById("cameras");
 
 // stream = video + audio
+// stream은 track을 제공하고 접근 가능하다.
 let myStream;
 let isMuted = false;
 let isCameraOn = false;
+
+async function getCameras() {
+  try {
+    const devices = await navigator.mediaDevices.enumerateDevices();
+    const cameras = devices.filter((device) => device.kind === "videoinput");
+    cameras.forEach((cam) => {
+      const $option = document.createElement("option");
+      $option.value = cam.deviceId;
+      $option.innerText = cam.label;
+      $cameraSelect.appendChild($option);
+    });
+    console.log(cameras);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
 async function getMedia() {
   // getUserMedia는 유저의 유저미디어 string을 준다
   try {
@@ -17,6 +36,7 @@ async function getMedia() {
       video: true,
     });
     $myFace.srcObject = myStream;
+    await getCameras();
   } catch (error) {
     console.log(error);
   }
@@ -25,6 +45,9 @@ async function getMedia() {
 getMedia();
 
 function handleMuteClick() {
+  myStream
+    .getAudioTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (!isMuted) {
     $muteBtn.innerText = "Unmute";
     isMuted = true;
@@ -34,6 +57,9 @@ function handleMuteClick() {
   }
 }
 function handleCameraClick() {
+  myStream
+    .getVideoTracks()
+    .forEach((track) => (track.enabled = !track.enabled));
   if (isCameraOn) {
     $cameraBtn.innerText = "Camera Off";
     isCameraOn = false;
